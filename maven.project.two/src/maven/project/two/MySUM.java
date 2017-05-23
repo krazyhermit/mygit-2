50b288d93a7906332222ec5f9a7b6e7a99014f10
Mohammad N Haq <myemail@myemail.com>
2017-05-23 19:02:09 -0400

    Initial Commit

---
+++ maven.project.two/src/maven/project/two/MySUM.java
@@ -0,0 +1,13 @@
+package maven.project.two;
+
+import maven.project.one.one.HelloMaven;
+
+public class MySUM {
+
+	public static void main(String[] args) {
+		HelloMaven testSum = new HelloMaven();
+		System.out.println("The sum is "+testSum.sum(2, 3));
+		testSum.printMethod();
+		System.out.println("test from 'MySUM' class");
+	}
+}
